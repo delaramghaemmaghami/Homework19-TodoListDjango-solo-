cf0e90389751a11b430d4903fdfffd5b99400956
DelaramGhaemmaghami <delaramsadatghaemmaghami1997@gmail.com>
2021-11-24 10:43:42 +0330

    Link to admin page for navbar

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", welcome_page, name="welcome_page")
+    path("", welcome_page, name="welcome_page"),
+    path('', TaskList.as_view(), name="task_list")
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
+from .models import *
 
 
 def welcome_page(request):
     return render(request, "welcomepage.html")
+
+
+class TaskList(ListView):
+    model = Task
+    template_name = "todo/todo_detail.html"
